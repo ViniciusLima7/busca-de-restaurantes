97e480555bfd76255932aad48319c102e0cb1fee
Marcos Vinicius Lima de França <mfranca@nelogica.com.br>
2021-10-19 13:30:51 -0300

    Adicionar ícone de Busca e Formatação do Menu Lateral

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,14 +1,34 @@
 import React from 'react'
-import { Container } from './styles'
+import { Container, Search, Logo, Wrapper, Map } from './styles'
+import logo from '../../assets/logo.svg';
+import TextField, { Input } from '@material/react-text-field';
+import MaterialIcon from '@material/react-material-icon';
+import { useState } from 'react';
+
 
 const Home = () => {
 
+    const [inputValue, setInputValue] = useState('');
+
     return (
-        <>
+        <Wrapper>
             <Container>
-                Home
+                <Search>
+                    <Logo src={logo} alt="logo do restaurante" />
+                    <TextField
+                        label='Pesquisar Retaurantes'
+                        outlined
+                        trailingIcon={<MaterialIcon role="button" icon="search" />}
+                    ><Input
+                            value={inputValue}
+                            onChange={(e) => setInputValue(e.target.value)} />
+                    </TextField>
+                </Search>
             </Container>
-        </>
+            <Map />
+
+
+        </Wrapper>
     );
 }
 
